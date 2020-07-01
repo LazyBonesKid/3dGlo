7ada035d9c06a3ecd40c6f4775ca930476440441
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-07-01 20:57:03 +0500

    Toggle_19

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('DOMContentLoaded', () => {
-
+    // Таймер
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
@@ -21,22 +21,86 @@ window.addEventListener('DOMContentLoaded', () => {
             seconds =  checkB(Math.floor(timeRemaining % 60)),
             minutes =   checkB(Math.floor((timeRemaining / 60) % 60)),
             hours =  checkB(Math.floor(timeRemaining / 60 / 60) % 24);
-            console.log('seconds: ', seconds);
-            console.log('minutes: ', minutes);
-            console.log('hours: ', hours);
             //day = Math.floor(timeRemaining / 60 / 60 / 24);
             return { timeRemaining, hours, minutes, seconds };
         };
+        const idTime = setInterval(updateClock, 1000);
 
-        const updateClock = () => {
-            let timer = getTimeRemaining();
-            timerHours.textContent = +timer.hours > 0 ? timer.hours : '00';
-            timerMinutes.textContent = +timer.minutes > 0 ? timer.minutes : '00';
-            timerSeconds.textContent = +timer.seconds > 0 ? timer.seconds : '00';
-        };
+        function updateClock() {
+            const timer = getTimeRemaining();
+            if (isNaN(+timer.hours)) {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                clearInterval(idTime);
+            } else {
+                timerHours.textContent = timer.hours;
+                timerMinutes.textContent = timer.minutes;
+                timerSeconds.textContent = timer.seconds;
+            }
+        }
         updateClock();
-        setInterval(updateClock, 1000);
+        idTime;
     }
+    countTimer('2 july 2020');
 
-    countTimer('1 july 2020');
+    // Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+            const handlerMenu = () => {
+                menu.classList.toggle('active-menu');
+            };
+
+            btnMenu.addEventListener('click', handlerMenu);
+            closeBtn.addEventListener('click', handlerMenu);
+            menuItems.forEach(item => { item.addEventListener('click', handlerMenu); });
+
+    };
+
+    toggleMenu();
+
+    const popupContent = document.querySelector('.popup-content');
+
+    const animationTogglePopUp = () => {
+        //const start = Date.now(); // запомнить время начала
+        let left = 0;
+        const timer = setInterval(() => {
+            if (popupContent.style.left !== '38%') {
+                popupContent.style.left = left + '%';
+                left++;
+            } else {
+                clearInterval(timer);
+                return;
+            }
+        }, 5);
+    };
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        popupBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || screen.width > 786) {
+                    animationTogglePopUp();
+                }
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+            if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || screen.width > 786) {
+                popupContent.style.left = '0%';
+            }
+        });
+    };
+
+    togglePopUp();
 });
+
