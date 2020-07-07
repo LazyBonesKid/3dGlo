3af81febf6015181f4560cd903546f5bbb179a83
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-07-07 15:25:19 +0500

    Calc24

--- script/script.js
+++ script/script.js
@@ -67,7 +67,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabContent = document.querySelectorAll('.service-tab');
 
             document.addEventListener('click', event => {
-                let target = event.target;
+                const target = event.target;
                 //меню
                 if (target.closest('.menu') || (target.closest('menu') && target.closest('a') !== null)) {
                     menu.classList.toggle('active-menu');
@@ -251,5 +251,85 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     inputNumber();
+
+    //калькулятор
+
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcCount = document.querySelector('.calc-count'),
+        calcDay = document.querySelector('.calc-day'),
+        totalValue = document.getElementById('total');
+
+
+        function animate({ timing, draw, duration }) {
+
+            const start = performance.now();
+
+            requestAnimationFrame(function animate(time) {
+
+                let timeFraction = (time - start) / duration;
+                if (timeFraction > 1) timeFraction = 1;
+
+                const progress = timing(timeFraction);
+
+                draw(progress);
+
+                if (timeFraction < 1) {
+                requestAnimationFrame(animate);
+                }
+            });
+        }
+
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const squareValue = +calcSquare.value,
+            typeValue = calcType.options[calcType.selectedIndex].value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue = 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue = 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = Math.trunc(price * typeValue * squareValue * countValue * dayValue);
+            } else {
+                total = 0;
+            }
+
+            const copyTotalValue = total;
+
+                animate({
+                    duration: 1000,
+                    timing: timeFraction => timeFraction,
+                    draw: progress => {
+                        totalValue.textContent = Math.trunc(copyTotalValue * progress);
+                    }
+                });
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+
+        });
+    };
+
+
+    calc(100);
 });
 
