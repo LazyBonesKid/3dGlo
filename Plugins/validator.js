b7ef7a994292540406e0c6a5431e30b2cf70e118
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-07-09 00:39:41 +0500

    Валидатор

---
+++ Plugins/validator.js
@@ -0,0 +1,121 @@
+// eslint-disable-next-line no-unused-vars
+class Validator {
+    constructor({ selector, pattern = {}, method }) {
+        this.form = document.querySelector(selector);
+        this.pattern = pattern;
+        this.method = method;
+        this.elementsForm = [...this.form.elements].filter(item => item.tagName.toLowerCase() !== 'button' && item.type !== 'button');
+        this.error = new Set();
+    }
+
+    init() {
+
+        this.applyStyle();
+        this.setPatter();
+        this.elementsForm.forEach(elem => elem.addEventListener('change', this.chekIt.bind(this)));
+        this.form.addEventListener('submit', e => {
+            this.elementsForm.forEach(elem => this.chekIt({target: elem}));
+            if (this.error.size) {
+                e.preventDefault();
+            }
+        });
+    }
+
+    isValid(elem) {
+        const validatorMethod = {
+            notEmpty(elem) {
+                if (elem.value.trim === '') {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            pattern(elem, pattern) {
+                return pattern.test(elem.value);
+            }
+        };
+
+        if (this.method) {
+            const method = this.method[elem.id];
+            if (method) {
+                return method.every(item => validatorMethod[item[0]](elem, this.pattern[item[1]]));
+            }
+        } else {
+            console.warn('Необходимо передать id полей ввода и методы проверки этих полей');
+        }
+
+        return true;
+    }
+
+    chekIt(event) {
+        const target = event.target;
+        
+        if (target.value.trim() === '') {
+            if (target.classList.contains('success')) {
+                target.classList.remove('success');
+            }
+            if (target.classList.contains('error')) {
+                target.classList.remove('error');
+            }
+            this.error.delete(target);
+            if (target.nextElementSibling  && target.nextElementSibling.classList.contains('validator-error')) {
+                target.nextElementSibling.remove();
+            }
+        } else {
+            if (this.isValid(target)) {
+                this.showSuccess(target);
+                this.error.delete(target);
+            } else {
+                this.showError(target);
+                this.error.add(target);
+            }
+        }
+    }
+
+    showError(elem) {
+        elem.classList.remove('success');
+        elem.classList.add('error');
+        if (elem.nextElementSibling !== null && elem.nextElementSibling.classList.contains('validator-error')) {
+            return;
+        }
+        const errorDiv = document.createElement('div');
+        errorDiv.textContent = 'Ошибка в этом поле';
+        errorDiv.classList.add('validator-error');
+        elem.insertAdjacentElement('afterend', errorDiv);
+    }
+
+    showSuccess(elem) {
+        elem.classList.remove('error');
+        elem.classList.add('success');
+        if (elem.nextElementSibling  && elem.nextElementSibling.classList.contains('validator-error')) {
+            elem.nextElementSibling.remove();
+        }
+    }
+
+    applyStyle() {
+        const style = document.createElement('style');
+        style.textContent = `
+        input.success {
+            border: 2px solid green
+        }
+        input.error {
+            border: 2px solid red
+        }
+        .validator-error {
+            front-size: 12px;
+            font-family: sans-serif;
+            color: red
+        }
+        `;
+        document.head.appendChild(style);
+    }
+
+    setPatter() {
+        if (!this.pattern.phone) {
+            this.pattern.phone = /^\+?[78]([-()]*\d){10}$/;
+        }
+        if (!this.pattern.email) {
+            this.pattern.email = /^\w+@\w+\.\w{2,}$/;
+        }
+    }
+}
